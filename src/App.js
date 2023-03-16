ecbcb2a72e7b97eaac02bc70a228613dd56009df
SFarida <faridagueleu@gmail.com>
2023-03-16 16:41:26 +0100

    Change div cards on home page to links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Countries from './components/Countries';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/" element={<Countries />} />
-          {/* <Route path="air-pollution-statistics/:city" element={<PollutionDetail />} /> */}
+          <Route path="country-details/:code" element={<CountryDetails />} />
         </Routes>
       </div>
       <Footer />
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Country = ({ name, code }) => (
-  <div className="card card-cover h-100 overflow-hidden rounded-4 shadow-lg card_item">
+  <Link to={`country-details/${code}`} className="card card-cover h-100 overflow-hidden rounded-4 shadow-lg card_item">
     <div className="card-body">
       <h4>{name}</h4>
       <p>{code}</p>
     </div>
-  </div>
+  </Link>
 );
 
 Country.propTypes = {
